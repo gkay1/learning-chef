1c9243e1376246ea394c900629ad5e60b0e67335
gkay1 <guilherme.imai@yahoo.com.br>
2014-10-21 16:56:36 +0900

    Gabija123

---
+++ benchmarks.rb
@@ -0,0 +1,9 @@
+           #vou comer a gabija
+         #sem arquivos teminados em .a
+            *.a
+          # mas rastreie lib.a,mesmo que voce tenha ignorado arquivos terminados em .a acima !lib.a
+           # apenas ignore o arquivo TODO nraiz, nao o subdiretorio TODO/TODO
+
+        #ignore todos os arquivos no diretorio build/
+      #ignore doc/notes.txt mas,nao ginore doc/server/arch.txt doc/*.txt
+# test line
